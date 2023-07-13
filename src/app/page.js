7a0423618ec3a12b9bff0ebaf84c23c815e24ba6
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-07-13 21:46:47 +0700

    feat: add form step one UI

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,49 @@ export default function Home() {
             <div className="form__subtitle">
               Please provide your personal info to help us deliver your gift
             </div>
-            <div className="form__step-1"></div>
+            <form className="form__input form__input--step-1" id="form-step-1">
+              <label htmlFor="name">
+                <div className="input__label">Name</div>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="e.g. Naufal Zufar"
+                  className="input__text"
+                />
+              </label>
+              <label htmlFor="number">
+                <div className="input__label">Phone Number</div>
+                <input
+                  type="tel"
+                  name="number"
+                  id="number"
+                  placeholder="e.g. +6281372961321"
+                  className="input__text"
+                />
+              </label>
+              <label htmlFor="email">
+                <div className="input__label">Email Adress</div>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="e.g. hansolo@mail.com"
+                  className="input__text"
+                />
+              </label>
+              <label htmlFor="address">
+                <div className="input__label">Delivery Adress</div>
+                <textarea
+                  type="text"
+                  name="address"
+                  id="address"
+                  placeholder="e.g. Gangnam street, Busan, 14020"
+                  className="input__textarea"
+                  rows={5}
+                />
+              </label>
+            </form>
           </div>
           <div className="btn-container">
             <button className="btn btn--back">Step Back</button>
