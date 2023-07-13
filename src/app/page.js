0d07f9372f6c065a3e576f5c65b5243ee420acee
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-07-13 20:01:52 +0700

    feat: add stepper component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,33 @@
+import stepData from "json/step.json";
+
+const Step = ({ isLastOrder, index, title }) => (
+  <div className="step">
+    <div className="step-status">
+      <div className="step-status__circle">
+        <div className="step-status__circle-inner"></div>
+      </div>
+      {!isLastOrder && (
+        <div className="step-status__connector">
+          <div className="step-status__connector-progress"></div>
+        </div>
+      )}
+    </div>
+    <div className="step-info">
+      <div className="step-info__order">Step {index + 1}</div>
+      <div className="step-info__title">{title}</div>
+    </div>
+  </div>
+);
+
+const stepper = stepData.data.map((step, index) => (
+  <Step
+    key={step.id}
+    isLastOrder={index + 1 === stepData.data.length}
+    index={index}
+    title={step.title}
+  />
+));
+
 export default function Home() {
   return (
     <div className="container">
@@ -5,20 +35,14 @@ export default function Home() {
         <section className="sidebar">
           <div className="sidebar__desktop-wrapper">
             <h1 className="brand-title">Time to Shopping</h1>
-            <div className="stepper stepper--desktop">
-              <div className="stepper__step"></div>
-              <div className="stepper__connector"></div>
-            </div>
+            <div className="stepper stepper--desktop">{stepper}</div>
             <img
               src="/images/gift-box.svg"
               className="brand-img"
               alt="brand title"
             />
           </div>
-          <div className="stepper stepper--mobile">
-            <div className="stepper__step"></div>
-            <div className="stepper__connector"></div>
-          </div>
+          <div className="stepper stepper--mobile">{stepper}</div>
         </section>
         <section className="form-container">
           <div className="form">
