452b00dfed7c140e02008f4a242f6f682d921260
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-07-14 17:16:23 +0700

    feat: add form step four UI

--- src/app/page.js
+++ src/app/page.js
@@ -173,7 +173,7 @@ export default function Home() {
                 />
               </label>
             </form> */}
-            <form className="form__input form__input--step-3" id="form-step-3">
+            {/* <form className="form__input form__input--step-3" id="form-step-3">
               <div>
                 <input
                   type="radio"
@@ -230,6 +230,26 @@ export default function Home() {
                     <div className="shipment-info__price">$35</div>
                   </div>
                 </label>
+              </div>
+            </form> */}
+            <form className="form__input form__input--step-4" id="form-step-4">
+              <div className="summary__container">
+                <div className="summary__item">
+                  <div className="item__title">Item Details</div>
+                  <div className="item__details">
+                    <div className="details__title">Teddy Bear</div>
+                    <div className="details__price">$80</div>
+                  </div>
+                  <hr className="item__divider"></hr>
+                  <div className="item__subtotal">
+                    <div className="subtotal__title">Subtotal</div>
+                    <div className="subtotal__price">$160</div>
+                  </div>
+                </div>
+                <div className="summary__total">
+                  <div className="total__title">Total</div>
+                  <div className="total__price">$500</div>
+                </div>
               </div>
             </form>
           </div>
