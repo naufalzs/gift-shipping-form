6825c6553fdcda48ec1d549baf5a7bcd95faeb5a
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-07-18 17:38:10 +0700

    feat: create stepping function

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
 "use client";
+import StepOne from "components/StepOne";
+import StepTwo from "components/StepTwo";
+import StepThree from "components/StepThree";
+import StepFour from "components/StepFour";
+import StepFive from "components/StepFive";
 import stepData from "json/step.json";
+import { useState } from "react";
+import cx from "classnames";
 
 const Step = ({ isLastOrder, index, title }) => (
   <div className="step">
@@ -20,6 +27,35 @@ const Step = ({ isLastOrder, index, title }) => (
   </div>
 );
 
+const CurrentForm = ({ currentStep }) => {
+  const MainForm = () => {
+    switch (currentStep) {
+      case 0:
+        return <StepOne />;
+      case 1:
+        return <StepTwo />;
+      case 2:
+        return <StepThree />;
+      case 3:
+        return <StepFour />;
+      case 4:
+        return <StepFive />;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="form">
+      <div className="form__title">{stepData.data[currentStep]?.title}</div>
+      <div className="form__subtitle">
+        {stepData.data[currentStep]?.description}
+      </div>
+      <MainForm />
+    </div>
+  );
+};
+
 const stepper = stepData.data.map((step, index) => (
   <Step
     key={step.id}
@@ -30,6 +66,20 @@ const stepper = stepData.data.map((step, index) => (
 ));
 
 export default function Home() {
+  const firstStep = 0;
+  const lastStep = 4;
+  const [currentStep, setCurrentStep] = useState(firstStep);
+
+  const handleNext = () => {
+    if (currentStep >= lastStep) return;
+    setCurrentStep(currentStep + 1);
+  };
+
+  const handleBack = () => {
+    if (currentStep <= firstStep) return;
+    setCurrentStep(currentStep - 1);
+  };
+
   return (
     <div className="container">
       <main className="card-wrapper">
@@ -46,16 +96,25 @@ export default function Home() {
           <div className="stepper stepper--mobile">{stepper}</div>
         </section>
         <section className="form-container">
-          <div className="form">
-            <div className="form__title">Shipping Info</div>
-            <div className="form__subtitle">
-              Please provide your personal info to help us deliver your gift
-            </div>
-            {/* function switch Step */}
-          </div>
+          <CurrentForm currentStep={currentStep} />
           <div className="btn-container">
-            <button className="btn btn--back">Step Back</button>
-            <button className="btn btn--next" form="form-step-1" type="submit">
+            <button
+              className={cx("btn btn--back", {
+                "btn--hide":
+                  currentStep === firstStep || currentStep === lastStep,
+              })}
+              onClick={handleBack}
+            >
+              Step Back
+            </button>
+            <button
+              className={cx("btn btn--next", {
+                "btn--hide": currentStep === lastStep,
+              })}
+              form="form-step-1"
+              type="submit"
+              onClick={handleNext}
+            >
               Next Step
             </button>
           </div>
