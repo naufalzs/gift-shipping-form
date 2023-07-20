0ad9206e7d0610e639b46b9a190f3d3d833a56ff
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-07-20 15:52:14 +0700

    feat(step-four): create summary item function, polish styling

--- components/StepFour.js
+++ components/StepFour.js
@@ -35,6 +35,32 @@ export default function StepFour() {
     handleNext();
   };
 
+  const transformKey = (name) => {
+    let modName = name;
+    modName = modName.replace("addon", "");
+    modName = modName.replaceAll("-", " ");
+    modName = modName.trim();
+    return modName;
+  };
+
+  const createGroup = (initialObject) => {
+    const mapFn = (key, value) => ({ name: transformKey(key), price: value });
+    const listObj = (obj, mapFn) =>
+      Object.entries(obj).reduce((acc, [key, value]) => {
+        acc.push(mapFn(key, value));
+        return acc;
+      }, []);
+
+    return {
+      data: listObj(initialObject, mapFn).filter((item) => item.price > 0),
+    };
+  };
+
+  const giftMessage = formInput.addOn["gift-card"];
+  delete formInput.addOn["gift-card"];
+
+  const groupAddon = createGroup(formInput?.addOn)?.data;
+  const groupShipment = formInput?.shipment;
   return (
     <form
       onSubmit={onSubmit}
@@ -44,6 +70,8 @@ export default function StepFour() {
       <div className="summary__container">
         <div>
           <SummaryItem title="Item" data={chosenItem?.data} />
+          <SummaryItem title="Add On" data={groupAddon} />
+          <SummaryItem title="Delivery" data={groupShipment} />
         </div>
         <div className="summary__total">
           <div className="total__title">Total</div>
