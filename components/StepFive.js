47389a012ebd443986210a1d537b9a91274075cd
Naufal Zufar S <naufalzufars@Naufals-MacBook-Pro.local>
2023-07-21 22:57:44 +0700

    feat(step-five): change thank you message wording

--- components/StepFive.js
+++ components/StepFive.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { StepContext } from "@/app/page";
 
 export default function StepFive() {
-  const {formInput} = useContext(StepContext)
+  const { formInput } = useContext(StepContext);
 
   return (
     <form className="form__input form__input--step-5" id="form-step-5">
@@ -18,10 +18,10 @@ export default function StepFive() {
           height={100}
         />
         <div className="finished__message">
-          Your order has been successfully completed. Throughout the process you
-          can check your order shipment step on your email. Thanks to trusted us
-          as your gift partner. If you need additional support, please kindly
-          email us at support@giftwrapper.com
+          Your order has been successfully completed. Please kindly wait, as we
+          are going to send your receipt and order tracking link via email.
+          Thanks for becoming our gift partner. If you need additional support,
+          please kindly email us at support@giftwrapper.com.
         </div>
       </div>
     </form>
